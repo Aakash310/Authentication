a8a9a1342b18bdb3e9ad900634b191a818f1af9e
Aakash310 <54284599+Aakash310@users.noreply.github.com>
2020-04-06 02:13:02 +0530

    Create secret.js

---
+++ AuthDemo/views/secret.js
@@ -0,0 +1,10 @@
+<h1>
+	This is the Secret Page!
+</h1>
+<p>
+	You found me!
+</p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS2hE_g_-wIHO_cDALhWiDMovD4BagDdvC3Incvww3KXfel7Jmj&usqp=CAU">
+<li><a href="/register">Sign UP</a></li>
+<li><a href="/login">LogIn</a></li>
+<li><a href="/logout">LogOut</a></li>
